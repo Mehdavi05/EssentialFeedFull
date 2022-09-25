96fda6ed9a0cc2bfc8497e3d84e897100a04ba52
Shujaat Hussain <shujaat.apple05@gmail.com>
2022-09-25 21:54:48 +0400

    Extract FailableInsertFeedStoreSpecs checks into reusable assertions

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/XCTestCase+FailableInsertFeedStoreSpecs.swift
@@ -0,0 +1,26 @@
+//
+//  XCTestCase+FailableInsertFeedStoreSpecs.swift
+//  EssentialFeedTests
+//
+//  Created by Shujaat-MacBook on 9/25/22.
+//
+
+import XCTest
+
+import XCTest
+
+import EssentialFeed
+
+extension FailableInsertFeedStoreSpecs where Self: XCTestCase {
+    func assertThatInsertDeliversErrorOnInsertionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        let insertionError = insert((uniqueImageFeed().local, Date()), to: sut)
+
+        XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error", file: file, line: line)
+    }
+    
+    func assertThatInsertHasNoSideEffectsOnInsertionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        insert((uniqueImageFeed().local, Date()), to: sut)
+        
+        expect(sut, toRetrieve: .empty, file: file, line: line)
+    }
+}
